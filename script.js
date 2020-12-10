fe85586c1761b54bbfe9f0de517ca9cdd56a57cb
bobbykwong <bobby.kwong@anxingtech.com>
2020-12-10 14:26:29 +0800

    update for better formatting

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const jsonfile = require('jsonfile')
-const masterList = './masterlist.json'
-const merchantList = './merchant-list-household.json'
+const newMasterList = './newMasterlist.json'
+const merchantList = './merchant-hair.json'
+const newMerchantList = './master-hair.json'
 const axios = require('axios');
 
+/*
 // Merchant API
 merchant_base_url = "https://gaigai.com"
 merchant_consumer_key = "ck_20d4c499f10909d53015ce28f0ab657b60e8488c"
 merchant_consumer_secret = "cs_487070c9bad8d1f1f2856c4aeddbc663bb53fe61"
-/*
+
 merchant_api = API(
     url=merchant_base_url,
     consumer_key=merchant_consumer_key,
@@ -15,7 +17,7 @@ merchant_api = API(
     version="wc/v3",
     timeout=20
 )
-*/
+
 
 let productNames = [];
 const file = './newMasterList.json'
@@ -69,51 +71,86 @@ const getAllProducts = () => {
 }
 
 getAllProducts();
-/*
-jsonfile.readFile(masterList)
+*/
+
+jsonfile.readFile(newMasterList)
 .then( obj => {
     let masterListArray = [];
-    // Put all product objects into an array
-    const keys = Object.keys(obj);
-    keys.forEach(key => {
-        masterListArray.push(obj[key])
-    });
-  
-    let masterProductArray = [];
-    masterListArray.forEach(element => {
-        const name = element["Name"].split(" ")
-        masterProductArray.push(name)
-    });
+
+    const allProductsMaster = obj["pageNumber"];
     
+    let productNamesArrayMaster = [];
+    // Split all product Names 
+    allProductsMaster.forEach(element => {
+        productNamesArrayMaster.push(element.split(" "));
+    });
+
+    // console.log(productNamesArrayMaster);
+
     let merchantProductList = []
     jsonfile.readFile(merchantList)
     .then(merchantObj => {
-        const merchantkeys = Object.keys(obj);
+        const merchantkeys = Object.keys(merchantObj);
         merchantkeys.forEach(key => {
-            merchantProductList.push(obj[key])
+            merchantObj[key]["id"] = key
+            merchantProductList.push(merchantObj[key])
         });
     
-        let merchantProductArray = [];
+        let productNamesArrayMerchant = [];
         merchantProductList.forEach(element => {
-            const name = element["Name"].split(" ")
-            merchantProductArray.push(name)
+            const id = element["id"] 
+            const price = element["Selling Price"].toString()
+            let name = element["Name"].split(" ")
+            name.push(price);
+            name.push(id);
+            productNamesArrayMerchant.push(name)
         });
+        console.log(productNamesArrayMerchant)
 
-
+        let results = []
         // Do matching
-        // Check first word which is typically brand name
-        for (let indexMer = 0; indexMer < merchantProductArray.length; indexMer++) {
-            const merchantProductFirstWord = merchantProductArray[indexMer][0].toLowerCase();
+        for (let indexMer = 0; indexMer < productNamesArrayMerchant.length; indexMer++) {
+            let optionsArray = [];
+            const merchantProductFirstWord = productNamesArrayMerchant[indexMer][0].toUpperCase();
             
-            for (let indexMas = 0; indexMas < masterProductArray.length; indexMas++) {
-                const masterProductFirstWord = masterProductArray[indexMas][0].toLowerCase();
+            for (let indexMas = 0; indexMas < productNamesArrayMaster.length; indexMas++) {
+                const masterProductFirstWord = productNamesArrayMaster[indexMas][0].toUpperCase();
                 
+                // Check first word which is typically brand name
                 if(merchantProductFirstWord == masterProductFirstWord){
-                    console.log(merchantProductFirstWord);
+                    let points = 0
+                    // Check if any other words match
+                    for (let indexProduct = 0; indexProduct < productNamesArrayMaster[indexMas].length; indexProduct++) {
+                        const currentWord = productNamesArrayMaster[indexMas][indexProduct];
+                        if(productNamesArrayMerchant[indexMer].includes(currentWord.toUpperCase())){
+                            points++;
+                        }
+                    }
+                    if(points === productNamesArrayMerchant[indexMer].length){
+                        results.push(productNamesArrayMerchant[indexMer][productNamesArrayMerchant[indexMer].length - 1])
+                        break;
+                    }
+                    else if(points >= 2 ){
+                        optionsArray.push(productNamesArrayMaster[indexMas].join(" "))
+                    }
+                }
+                else if(optionsArray.length === 0 && indexMas === productNamesArrayMaster.length - 1){
+                    results.push(false);
                 }
             }
+            
+            if(optionsArray.length > 0){
+                optionsArray.push(productNamesArrayMerchant[indexMer][productNamesArrayMerchant[indexMer].length - 1])
+                results.push(optionsArray)
+            }
         }
+        const data = {"results": results}
+        jsonfile.writeFile(newMerchantList, data)
+            .then(res => {
+                console.log("done")
+            })
+            .catch(err => {
+                console.log(err)
+            })
     })
 })
-
-*/
